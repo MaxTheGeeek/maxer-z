b54e6223d8562ae0aea2ea0dad168766cd7e3e95
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2026-06-07 11:02:13 +0200

    ADD: MCP backend config model

---
+++ src/MaxerZ.Api/Models/McpConfig.cs
@@ -0,0 +1,9 @@
+namespace MaxerZ.Api.Models
+{
+    public class McpConfig
+    {
+        public string McpBaseUrl { get; set; } = "";
+        public string McpApiKey { get; set; } = "";
+        public bool IsEnabled { get; set; } = false;
+    }
+}
